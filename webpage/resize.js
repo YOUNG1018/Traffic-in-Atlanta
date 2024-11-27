ad2bc26891f4b280f801fd4b22e8f678d0e98896
Yishu Ji <57058074+yishu-ji@users.noreply.github.com>
2024-11-27 11:39:25 -0500

    Update resize.js

--- webpage/resize.js
+++ webpage/resize.js
@@ -33,7 +33,7 @@ function initializeViz(id, tableauUrl, widthPercentage) {
 
 // You may cumstomize the size to your vis here
 document.addEventListener('DOMContentLoaded', function () {
-    initializeViz('viz1732719356408', 'shared/5D8CFGWCC', 0.5); // interactive map 1 - 50%
+    // initializeViz('viz1732719356408', 'shared/5D8CFGWCC', 0.5); // interactive map 1 - 50%
     initializeViz('viz1732723493241', 'shared/NMMKGZX32', 0.75); // interactive map 2 -75%
     initializeViz('viz1732722871550', 'Yixuan/Map_select', 0.6); 
     initializeViz('viz1732723104838', 'Yixuan/089-3323', 0.4); 
